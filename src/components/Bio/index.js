ecdf5b626983c561b4d42a4c917dbc5f5db70321
Anthony Scinocco <scinocco.a@gmail.com>
2018-01-28 18:08:53 -0500

    added some basic profile styles

--- src/components/Bio/index.js
+++ src/components/Bio/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import styles from './Bio.css';
+import profileImage from '../../assets/images/anthony-scinocco.jpg';
 
 const Bio = () => (
   <section className={styles.container}>
-    Bio
+    <header className={styles['profile-image--container']}>
+      <img
+        className={styles['profile-image']}
+        src={profileImage}
+        alt={'Anthony Scinocco | Full Stack Developer'}
+      />
+    </header>
+    <main className={styles['profile-description--container']}>
+      <h1 className={styles['profile-description--name']}>Anthony Scinocco</h1>
+      <h2 className={styles['profile-description--job-title']}>Full Stack Developer</h2>
+    </main>
   </section>
 );
 
