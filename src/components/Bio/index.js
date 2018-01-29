bd90244142079dfbd15e187c6db0e85553a7299f
Anthony Scinocco <scinocco.a@gmail.com>
2018-01-28 19:23:55 -0500

    added about me content and responsive spacing

--- src/components/Bio/index.js
+++ src/components/Bio/index.js
@@ -14,6 +14,27 @@ const Bio = () => (
     <main className={styles['profile-description--container']}>
       <h1 className={styles['profile-description--name']}>Anthony Scinocco</h1>
       <h2 className={styles['profile-description--job-title']}>Full Stack Developer</h2>
+      <div className={styles['profile-about-me']}>
+        <p>
+          Hi I&#39;m Anthony. Here is some text about.
+          This is my life. Woohoo, blah blah blah. Witty comment. Let&#39;s chat.
+        </p>
+        <h2>You can find me on:</h2>
+        <ul>
+          <li>
+            <a href={'https://google.ca'} className={styles['portfolio-link']}>LinkedIn</a>
+          </li>
+          <li>
+            <a href={'https://google.ca'} className={styles['portfolio-link']}>Github</a>
+          </li>
+          <li>
+            <a href={'https://google.ca'} className={styles['portfolio-link']}>Twitter</a>
+          </li>
+          <li>
+            <a href={'https://google.ca'} className={styles['portfolio-link']}>Docker Hub</a>
+          </li>
+        </ul>
+      </div>
     </main>
     <footer className={styles['profile-keywords--container']}>
       <span>Geek</span>
