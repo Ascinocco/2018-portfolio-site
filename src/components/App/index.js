74ac76bb166cba5adf3887ac932236ff09d57790
Anthony Scinocco <scinocco.a@gmail.com>
2018-02-04 17:23:25 -0500

    app.js structure and no modules is weird

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,36 +1,56 @@
 import React from 'react';
-// import { Grid, Segment } from 'semantic-ui-react';
-import { Segment, Responsive } from 'semantic-ui-react';
+import { Grid, Container } from 'semantic-ui-react';
+import './App.css';
 
-// const { Row, Column } = Grid;
-
-// const { Column } = Grid;
+const { Row, Column } = Grid;
 
 const App = () => (
-  <Segment.Group>
-    {/* <Responsive as={Segment} {...Responsive.onlyMobile}>Mobile</Responsive>
-    <Responsive as={Segment} {...Responsive.onlyTablet}>Tablet</Responsive>
-    <Responsive as={Segment} {...Responsive.onlyComputer}>Computer</Responsive>
-    <Responsive as={Segment} {...Responsive.onlyLargeScreen}>Large Screen</Responsive>
-    <Responsive as={Segment} {...Responsive.onlyWidescreen}>Widescreen</Responsive> */}
-    <Responsive as={Segment} {...Responsive}>Does this work everywhere?</Responsive>
-  </Segment.Group>
+  <Grid
+    container
+    columns={3}
+    stackable
+    stretched
+    textAlign={'left'}
+  >
+    <Row className={'portfolio--row'}>
+      <Column width={4}>
+        <h1>Bio</h1>
+      </Column>
+      <Column width={6}>
+        <h1>Experience &amp; Education</h1>
+      </Column>
+      <Column width={6}>
+        <h1>Github Feed</h1>
+      </Column>
+    </Row>
+    <Row className={'portfolio--row'}>
+      <Column width={5}>
+        <h1>Hobbies &amp; Interests</h1>
+      </Column>
+      <Column width={11}>
+        <Row>
+          <Container>
+            <h1>My Favourite Projects</h1>
+          </Container>
+        </Row>
+        <Row>
+          <Container>
+            <h1>Project One</h1>
+          </Container>
+        </Row>
+        <Row>
+          <Container>
+            <h1>Project Two</h1>
+          </Container>
+        </Row>
+        <Row>
+          <Container>
+            <h1>Project Three</h1>
+          </Container>
+        </Row>
+      </Column>
+    </Row>
+  </Grid>
 );
-
-// const App = () => (
-//   <Grid columns={'equal'}>
-//     {/* <Row> */}
-//     <Column width={4}>
-//       <Segment>Bio</Segment>
-//     </Column>
-//     <Column width={6}>
-//       <Segment>Github Feed</Segment>
-//     </Column>
-//     <Column width={6}>
-//       <Segment>Experience &amp; Education</Segment>
-//     </Column>
-//     {/* </Row> */}
-//   </Grid>
-// );
 
 export default App;
