beef9dae4bc2fb8c453a272533210f424e0954ee
Anthony Scinocco <scinocco.a@gmail.com>
2018-02-04 16:31:15 -0500

    some updates to support semantic ui

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,36 @@
 import React from 'react';
+// import { Grid, Segment } from 'semantic-ui-react';
+import { Segment, Responsive } from 'semantic-ui-react';
+
+// const { Row, Column } = Grid;
+
+// const { Column } = Grid;
 
 const App = () => (
-  <div>
-    <header>
-      <h1>Anthonys App</h1>
-    </header>
-  </div>
+  <Segment.Group>
+    {/* <Responsive as={Segment} {...Responsive.onlyMobile}>Mobile</Responsive>
+    <Responsive as={Segment} {...Responsive.onlyTablet}>Tablet</Responsive>
+    <Responsive as={Segment} {...Responsive.onlyComputer}>Computer</Responsive>
+    <Responsive as={Segment} {...Responsive.onlyLargeScreen}>Large Screen</Responsive>
+    <Responsive as={Segment} {...Responsive.onlyWidescreen}>Widescreen</Responsive> */}
+    <Responsive as={Segment} {...Responsive}>Does this work everywhere?</Responsive>
+  </Segment.Group>
 );
+
+// const App = () => (
+//   <Grid columns={'equal'}>
+//     {/* <Row> */}
+//     <Column width={4}>
+//       <Segment>Bio</Segment>
+//     </Column>
+//     <Column width={6}>
+//       <Segment>Github Feed</Segment>
+//     </Column>
+//     <Column width={6}>
+//       <Segment>Experience &amp; Education</Segment>
+//     </Column>
+//     {/* </Row> */}
+//   </Grid>
+// );
 
 export default App;
